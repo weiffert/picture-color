e9892a7084ac60ff145e42d861c0115e98441749
William Eiffert <weiffert@purdue.edu>
2022-05-13 18:16:42 -0400

    Sample images

--- color.py
+++ color.py
@@ -15,16 +15,22 @@ def main():
 
     print(f"Graphing {len(sample)} data points...")
     generateColorGraph(sample)
-    print(f"Done graphing. View file at {sys.argv[1].split('.')[0]}-graph.png")
+    print(f"Done graphing. View file at {plotOutput(sample)}")
 
 def readImage(path):
     image = Image.open(path)
     pixels = list(image.getdata())
     return pixels
 
+def title():
+    return sys.argv[1].split('.')[0]
+
+def plotOutput(data):
+    return f"{title()}-{len(data)}-graph.png"
+
 def getSamplePercentage():
     if len(sys.argv) == 3:
-        percentage = sys.argv[3]
+        percentage = float(sys.argv[2])
         if percentage > 1 or percentage <= 0:
             print("Invalid sample percentage. Must be > 0 and <= 1")
             exit(1)
@@ -37,8 +43,8 @@ def generateColorGraph(colorData):
 
     for (r, g, b) in colorData: 
         ax.scatter(r, g, b, color=colors.to_hex([r/255, g/255, b/255]), s=0.5)
-    plt.title(f"{sys.argv[1].split('.')[0]} Color Distribution")
-    plt.savefig(f"{sys.argv[1].split('.')[0]}-graph.png")
+    plt.title(f"{title()} Color Distribution of {len(colorData)} Data Points")
+    plt.savefig(plotOutput(colorData))
 
 def sampleData(data, percentage):
     return random.sample(
@@ -46,11 +52,7 @@ def sampleData(data, percentage):
         # Ensure doesn't exceed the number of pixels
         min(
             len(data),
-            # At least 5,000 data points
-            max(
-                math.floor(len(data) * percentage), 
-                5000
-            )
+            math.floor(len(data) * percentage), 
         )
     )
 
